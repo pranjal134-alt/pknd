3c00463fd73bbdfa600bb2ee3a4d09995fd33239
pranjal134-alt <sharmaharishankar700@gmail.com>
2026-02-12 15:06:11 +0530

    uy

--- palindrome.py
+++ palindrome.py
@@ -9,3 +9,12 @@ def is_palindrome(s):
     s=s.replace(" ", "").lower()
     return s == s[::-1]
 print(is_palindrome("anA"))
+import csv
+
+with open("csk.csv", "r") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+f=open('tv.csv','r') 
+data=f.read()
+print(data)       
--- str.py
+++ str.py
@@ -1,4 +1,15 @@
 str="apple"
 print(str.count("p"))
 str="apple"
-print(str.endswith("p"))
+print(str.endswith("p")) 
+str="apple"
+print(str.title())
+def cal_avg(num1,num2):
+    avg=(num1+num2)/2
+    return avg
+num1=10
+num2=20
+average=cal_avg(num1,num2)
+print("The average is:", average)
+a=123
+print("123".isalnum())
